5b6e6b5795a0fe2d78e171925a118db89140d58b
Valentino Ekaputra <silentrazgriz@gmail.com>
2018-10-09 11:54:58 +0700

    change title

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ $title ?? 'Tekton' }}</title>
+    <title>{{ $title ?? config('app.name') ?? 'Tekton' }}</title>
     @yield('tekton-styles')
     @include('components.styles')
     @yield('styles')
